dc2f76d4867311b0acdf6fbf4665f545797e7a97
Amit Vashist <amitvashist7@outlook.com>
2022-09-16 06:44:35 +0000

    18-RemoteBackend-and-Remote-State

---
+++ 18-RemoteBackend-and-Remote-State/Project-AV/terraform.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.20"
+    }
+  }
+
+## Local Backend Config ##
+  /*
+  backend "local" { 
+    path  = "/var/tmp/terraform-local-backend/terraform.tfstate"
+  }
+  */
+  
+## Remote Backend Config ##
+
+   backend "s3" {
+    bucket = "my-s3-bucket-av"
+    key    = "av.terraform.tfstate"
+    region = "us-east-2"
+  } 
+
+}
